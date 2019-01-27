bcbd56ae6ebdd98455d2b027c13f9135b2c56b14
Kinda <lamkinun@gmail.com>
2019-01-28 04:04:37 +0800

    Hello World!

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("I'm supercatex.")
+import cv2
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    success, frame = cap.read()
+    if not success:
+        print("Can't read frame from camera.")
+        break
+
+    cv2.imshow("WebCam", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
